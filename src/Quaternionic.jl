fa4fd38381004c155f289b343705e56ff31d95e5
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-05-18 11:23:10 -0400

    Simple converters and random quaternions

--- src/Quaternionic.jl
+++ src/Quaternionic.jl
@@ -2,6 +2,8 @@ module Quaternionic
 export Quaternion, Quat
 
 using StaticArrays, Latexify, LaTeXStrings
+import Random: AbstractRNG, default_rng
+
 
 struct Quaternion{T<:Real} <: Number
     components::SVector{4, T}
@@ -192,6 +194,63 @@ end
 
 
 Broadcast.broadcasted(f, q::Quaternion, args...) = Quaternion(f.(q.components, args...))
+
+
+"""
+    as_quat_array(A)
+
+View a real array as an array of quaternions
+
+The input array must have an initial dimension whose size is
+divisible by four (or better yet *is* 4), because successive
+indices in that last dimension will be considered successive
+components of the output quaternion.
+"""
+as_quat_array(A::AbstractArray{T}) where {T<:Real} = reinterpret(reshape, Quaternion{T}, A)
+
+
+"""
+    as_float_array(A)
+
+View a quaternion array as an array of real numbers
+
+This function is fast because no data is copied; the returned quantity is just a "view"
+of the original.
+
+The output view will have an extra initial dimension (of size 4), but is otherwise the
+same shape as the input array.
+
+"""
+as_float_array(A::AbstractArray{Quaternion{T}}) where {T} = reinterpret(reshape, T, A)
+
+
+function Base.randn(r::AbstractRNG, T::Type{Quaternion{S}}, dims::Tuple{Vararg{Int64, N}} where N) where {S}
+    random_array = randn(r, S, (4, dims...))
+    as_quat_array(random_array)
+end
+function Base.randn(r::AbstractRNG, T::Type{Quaternion{S}}) where {S}
+    Quaternion(randn(r, S, (4,)))
+end
+Base.randn(T::Quaternion{S}, dims::Tuple{Vararg{Int64, N}} where N) where {S} = Base.randn(default_rng(), T, dims...)
+Base.randn(T::Quaternion{S}) where {S} = Base.randn(default_rng(), T)
+
+function random_rotors(T::Type{Quaternion{S}}, normalize::Bool=true, dims::Tuple{Vararg{Int64, N}} where N=()) where {S}
+    q = randn(T, dims...)
+    if normalize
+        return q ./ abs.(q)
+    end
+    q
+end
+random_rotors(normalize::Bool, dims::Tuple{Vararg{Int64, N}} where N) = random_rotors(Quaternion{Float64}, normalize, dims)
+random_rotors(dims::Tuple{Vararg{Int64, N}} where N) = random_rotors(Quaternion{Float64}, true, dims)
+function random_rotors(T::Type{Quaternion{S}}, normalize::Bool=true) where {S}
+    q = randn(T)
+    if normalize
+        return q / abs(q)
+    end
+    q
+end
+random_rotors(normalize::Bool=true) = random_rotors(Quaternion{Float64}, normalize)
 
 
 end  # module
