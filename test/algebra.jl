d54123b15a2ae5ac4bb4f9dc51e8ef901af16901
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-09-17 15:18:48 -0400

    Fix test with workaround for Symbolics.jl/#379

--- test/algebra.jl
+++ test/algebra.jl
@@ -103,7 +103,9 @@ end
         end
         function next_quaternion!(chars)
             x = Symbol(popfirst!(chars))
-            xvar = @variables $x[1:4]
+            # Work around <https://github.com/JuliaSymbolics/Symbolics.jl/issues/379>:
+            # xvar = @variables $x[1:4]
+            xvar = [[Symbolics.variable(:x, i) for i in 1:4]]
             Quaternion(xvar[1]...)
         end
 
