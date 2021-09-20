eede9b18e4b1a12648294010d95cc98e4aec87e7
Michael Boyle <michael.oliver.boyle@gmail.com>
2021-09-20 11:44:47 -0400

    Add `stable` docs [skip tests]

--- docs/make.jl
+++ docs/make.jl
@@ -8,10 +8,8 @@ makedocs(
     format = Documenter.HTML(
         prettyurls = !("local" in ARGS),  # Use clean URLs, unless built as a "local" build
         edit_link = "main",  # Link out to "main" branch on github
-        #= UNCOMMENT ONCE STABLE:
         canonical = "https://moble.github.io/Quaternionic.jl/stable/",
         warn_outdated = true,
-        :UNCOMMENT ONCE STABLE =#
     ),
     pages = [
         "Introduction" => "index.md",
