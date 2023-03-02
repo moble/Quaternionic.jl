3c60d12722b253438bd3d86fc9fde49b9ab614f6
user.email <michael.oliver.boyle@gmail.com>
2023-03-02 16:08:52 -0500

    Add convenience scripts for docs and tests

---
+++ scripts/test.jl
@@ -0,0 +1,20 @@
+import Dates
+println("Running tests starting at ", Dates.format(Dates.now(), "HH:MM:SS"), ".")
+
+using Pkg
+cd((@__DIR__) * "/..")
+Pkg.activate(".")
+
+try
+    Δt = @elapsed Pkg.test("Quaternionic"; coverage=true, test_args=ARGS)
+    println("Running tests took $Δt seconds.")
+catch e
+    println("Tests failed; proceeding to coverage")
+end
+
+Pkg.activate()  # Activate Julia's base (home) directory
+using Coverage
+cd((@__DIR__) * "/..")
+coverage = Coverage.process_folder()
+Coverage.writefile("lcov.info", coverage)
+Coverage.clean_folder(".")
