630e3c274ae7e426ec6bab6944de21aa9265204c
Mike Boyle <michael.oliver.boyle@gmail.com>
2023-09-19 21:01:14 -0400

    Update repo specification [skip tests]

--- docs/make.jl
+++ docs/make.jl
@@ -6,12 +6,11 @@ DocMeta.setdocmeta!(Quaternionic, :DocTestSetup, :(using Quaternionic); recursiv
 makedocs(;
     modules=[Quaternionic],
     authors="Michael Boyle <michael.oliver.boyle@gmail.com>",
-    repo="https://github.com/moble/Quaternionic.jl/blob/{commit}{path}#{line}",
+    repo=GitHub("moble", "Quaternionic.jl"),
     sitename="Quaternionic.jl",
     format=Documenter.HTML(
         prettyurls=get(ENV, "CI", "false") == "true",
         canonical="https://moble.github.io/Quaternionic.jl/stable/",
-        assets=String[],
     ),
     pages=[
         "Introduction" => "index.md",
