75fc1b5491568c96894e86def50fdfe1ceb52d13
Mike Boyle <michael.oliver.boyle@gmail.com>
2024-10-21 16:32:40 -0400

    Debug what's happening in ChainRulesCoreExt

--- ext/QuaternionicChainRulesCoreExt.jl
+++ ext/QuaternionicChainRulesCoreExt.jl
@@ -12,7 +12,10 @@ isdefined(Base, :get_extension) ?
 # It's likely that StaticArrays will have its own ChainRulesCore extension someday, so we
 # need to check if there is already a ProjectTo defined for SArray.  If so, we'll use that.
 # If not, we'll define one here.
+@info [repr(method) for method in methods(ProjectTo) if occursin("SArray", repr(method.sig))]
+@show any(method->occursin("SArray", repr(method.sig)), methods(ProjectTo))
 if !any(method->occursin("SArray", repr(method.sig)), methods(ProjectTo))
+    @info "Defining ProjectTo for SArray"
     # These are ripped from https://github.com/JuliaArrays/StaticArrays.jl/pull/1068
     function (project::ProjectTo{<:Tangent{<:Tuple}})(dx::SArray)
         dy = reshape(dx, axes(project.elements))  # allows for dx::OffsetArray
