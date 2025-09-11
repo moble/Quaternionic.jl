805b791e18c7f8184e35de27c768501867b18f71
Mike Boyle <michael.oliver.boyle@gmail.com>
2025-09-11 09:52:30 -0400

    Explain what's going on a little more clearly

--- ext/QuaternionicForwardDiffExt.jl
+++ ext/QuaternionicForwardDiffExt.jl
@@ -3,9 +3,13 @@ module QuaternionicForwardDiffExt
 using Quaternionic
 isdefined(Base, :get_extension) ? (using ForwardDiff) : (using ..ForwardDiff)
 
-# Let ForwardDiff act naturally on quaternions.  This is cribbed from similar expressions
+###############################################
+# Let ForwardDiff act naturally on quaternions.
+
+# These next three definitions are cribbed from similar expressions
 # enabling differentiation of complex-valued functions in
 # https://github.com/JuliaDiff/ForwardDiff.jl/blob/78c73afd9a21593daf54f61c7d0db67130cf29e1/src/derivative.jl#L83-L88
+
 @inline ForwardDiff.extract_derivative(::Type{T}, y::AbstractQuaternion) where {T} = zero(y)
 
 # Both Quaternion and Rotor, when differentiated, result in a Quaternion...
